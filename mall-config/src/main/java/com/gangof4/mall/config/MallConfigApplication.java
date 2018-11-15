bb67a6c8b67aaeee64120a78a5383dd22472f829
congleiwang <congleiwang@qq.com>
2018-11-15 23:35:47 +0800

    add mall-config code

---
+++ mall-config/src/main/java/com/gangof4/mall/config/MallConfigApplication.java
@@ -0,0 +1,16 @@
+package com.gangof4.mall.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableConfigServer
+public class MallConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MallConfigApplication.class, args);
+    }
+}
---
+++ mall-config/src/test/java/com/gangof4/mall/config/MallConfigApplicationTests.java
@@ -0,0 +1,16 @@
+package com.gangof4.mall.config;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MallConfigApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
